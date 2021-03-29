1c1057ae3f3f2129bd53ce413fa9ebfb674f9c06
caoank61 <72555900+caoank61@users.noreply.github.com>
2021-03-30 00:41:50 +0700

    Create demo1.cpp

---
+++ demo1.cpp
@@ -0,0 +1,119 @@
+#include<stdio.h>
+#include<string.h>
+#include<windows.h>
+/*
+Y tuong them:
+	-Trong muc menu thay vi chi cho tim kiem bang the loai thi co the tim kiem bang ten
+	VD: Go "T" thi se list ra danh sach nhung cuon sach co chu T dau tien trong ten
+*/
+struct book_st{
+	char TenSach[30]; 
+	char TenTacGia[100];
+	char TheLoai[30]; 
+	int NamXuatBan;
+	int GiaTien;
+	};
+	typedef book_st Sach;
+void trangtri()
+{
+    int i;
+    for (i=0;i<=100;i=i+2) {
+        system("cls");
+        printf("\n\t\t\t    Dang tai chuong trinh");
+       	printf("\n\n\t\t__________________________________________");
+        printf("\n\n\t\t\t\t\Loading %d % ", i); 
+        Sleep(35);
+ }
+ 	Sleep(900);
+}
+
+void NhapThongTinSach(Sach &s1){
+	fflush(stdin);
+	printf("Nhap ten sach: ");
+	gets(s1.TenSach);
+	fflush(stdin);
+	printf("Nhap ten tac gia: ");
+	gets(s1.TenTacGia);
+	printf("Nhap nam xuat ban: ");
+	scanf("%d",&s1.NamXuatBan);
+	fflush(stdin);
+	printf("Nhap the loai: ");
+	gets(s1.TheLoai);
+	printf("Nhap gia tien: ");
+	scanf("%d", &s1.GiaTien);
+		
+}
+void NhapSoLuong(book_st s1[], int n){
+	for (int i=1;i<=n;i++){
+		printf("Cuon %d: \n",i);
+		NhapThongTinSach(s1[i]);
+	}
+}
+void XuatThongTinSach(book_st s1){
+		printf("\n\t\t\t ");
+	printf("\tTen sach: %s\n",s1.TenSach );
+		printf("\n\t\t\t ");
+	printf("\tTen tac gia: %s\n", s1.TenTacGia);
+		printf("\n\t\t\t ");
+	printf("\tNam xuat ban: %d\n",s1.NamXuatBan);
+		printf("\n\t\t\t ");
+	printf("\tThe loai: %s\n",s1.TheLoai);
+		printf("\n\t\t\t ");
+	printf("\tGia tien: %d\n",s1.GiaTien);
+	printf("\n-------------------------------------------------\n");
+}
+
+void XuatDS(book_st s1[], int n){
+	for(int i=1; i<=n;i++){
+		printf("\n\t\t\t ");
+		printf("      ~Thong tin cuon thu %d~ \n", i);
+		XuatThongTinSach(s1[i]);
+	}
+}
+void Menu(){
+	int chon, flat=1;
+	while(flat){
+		printf("-------------------------------------MENU-------------------------------------");
+		printf("\n\t\t\t ");
+		printf("1. Nhap  thong tin sach.\n");
+		printf("\t\t\t ");
+		printf("2. Thong tin sach.\n");
+		printf("\t\t\t ");
+		printf("3. Tim sach theo the loai.\n");
+		printf("\t\t\t ");
+		printf("4. Ghi vao tap tin book.dat.\n");
+		printf("\t\t\t ");
+		printf("5. Thoat.\n");
+		printf("============================Moi ban chon chuc nang============================");
+		printf("\n\t\t\t\t       ");
+		scanf("%d", &chon);
+		switch(chon){
+			case 1: 
+				struct book_st a[100];
+				int n;
+				printf ("\t\t      So cuon sach can nhap thong tin:  ");
+				scanf ("%d",&n);
+				NhapSoLuong(a,n);
+				printf("\n\t\t\t    ");
+				printf("Da nhap xong thong tin sach!");
+				Sleep(3000);
+				system("cls");
+				break;
+			case 2:
+				printf("\n\t\t\t\t ");
+				printf("Thong tin sach: \n");
+				XuatDS(a,n);
+				printf("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+				break;
+			case 3://dung so sanh chuoi 
+			case 4:
+			case 5:
+				flat=0;		
+		}
+	}
+}
+int main(){
+	//trangtri();	
+	system("cls");
+	Menu();
+}
